f21e038dd7d2e14fa03fe749c78912d05d919e04
kimtaemin <mini971018@gmail.com>
2023-01-06 15:46:05 +0900

    230105 수업내용 추가

--- APIApp/GameEngineCore/GameEngineCore.cpp
+++ APIApp/GameEngineCore/GameEngineCore.cpp
@@ -18,7 +18,9 @@ void GameEngineCore::GlobalUpdate()
 		MsgAssert("레벨을 지정해주지 않은 상태로 코어를 실행했습니다");
 		return;
 	}
-	
+
+	Core->MainLevel->ActorsUpdate();
+	Core->MainLevel->ActorsRender();
 }
 
 void GameEngineCore::GlobalEnd() 
@@ -30,6 +32,8 @@ void GameEngineCore::GlobalEnd()
 GameEngineCore::GameEngineCore() 
 {
 	GameEngineDebug::LeakCheck();
+	// 나는 자식중에 하나일수밖에 없다.
+	// 나는 절대만들어질수 없기 때문이다.
 	Core = this;
 }
 
@@ -67,4 +71,15 @@ void GameEngineCore::ChangeLevel(const std::string_view& _Name)
 	}
 
 	MainLevel = FindIter->second;
+}
+
+void GameEngineCore::LevelLoading(GameEngineLevel* _Level)
+{
+	if (nullptr == _Level)
+	{
+		MsgAssert("nullptr 인 레벨을 로딩하려고 했습니다.");
+		return;
+	}
+
+	_Level->Loading();
 }
